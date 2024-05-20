5a17eace1a5fe96c578d0becd679d6a48736bca9
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-20 14:28:53 +0900

    스페이서

--- Spacer/Spacer/ViewController.swift
+++ Spacer/Spacer/ViewController.swift
@@ -11,7 +11,36 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let labelLeft = UILabel()
+        labelLeft.text = "왼쪽"
+        labelLeft.backgroundColor = .lightGray
+        
+        let labelRight = UILabel()
+        labelRight.text = "오른쪽"
+        labelRight.backgroundColor = .lightGray
+        
+        let spacer = UIView()
+        spacer.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        spacer.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+        
+        stackView.addArrangedSubview(labelLeft)
+        stackView.addArrangedSubview(spacer)
+        stackView.addArrangedSubview(labelRight)
+        
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16)
+        ])
     }
 
 
