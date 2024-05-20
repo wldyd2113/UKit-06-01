0acdd09ff3e83207e71bf9a7d5ede6dc683d7d5a
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-20 16:28:25 +0900

    WebView 로 텍스트 그리기

--- Text/Text/ViewController.swift
+++ Text/Text/ViewController.swift
@@ -6,24 +6,53 @@
 //
 
 import UIKit
-
+import WebKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let attributedString = NSMutableAttributedString(string: "Hello World")
-        attributedString.addAttribute(.font, value: UIFont.italicSystemFont(ofSize: 20), range: NSRange(location: 0, length: 5))
-        attributedString.addAttribute(.foregroundColor, value: UIColor.blue, range: NSRange(location: 7, length: 5))
-        attributedString.addAttribute(.underlineStyle, value: NSUnderlineStyle.single.rawValue, range: NSRange(location: 0, length: 12))
+        let webView = WKWebView()
         
-        let label = UILabel()
-        label.attributedText = attributedString
-        label.translatesAutoresizingMaskIntoConstraints = false
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        let htmlString = """
+        <html>
+        <head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <style>
+                body {
+                    font-family: -apple-system, Helvetica, Arial, sans-serif;
+                    font-size: 2rem;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                }
+                .italic {
+                    font-style: italic;
+                }
+                .blue {
+                    color: blue;
+                }
+                .underline {
+                    text-decoration: underline;
+                }
+                
+            </style>
+        </head>
+        <body>
+            <span class="italic underline">Hello,&nbsp;</span><span class="blue underline">World</span>
+        </body>
+        </html>
+        """
+        webView.loadHTMLString(htmlString, baseURL: nil)
+        
+        view.addSubview(webView)
         
         NSLayoutConstraint.activate([
-            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-                        label.topAnchor.constraint(equalTo: view.topAnchor, constant: 200)
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
         
     }
