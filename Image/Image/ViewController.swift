c2ead2529b1c917086dc8e44ebc4e6bb10d2e353
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-20 17:28:08 +0900

    Clear

--- Image/Image/ViewController.swift
+++ Image/Image/ViewController.swift
@@ -11,33 +11,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let circleView = UIView()
-        circleView.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
-        circleView.backgroundColor = .yellow
-        circleView.layer.cornerRadius = 50
-        circleView.layer.borderColor = UIColor.black.cgColor
-        circleView.layer.borderWidth = 2
-        circleView.clipsToBounds = true
-
-        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 20))
-        label.text = "Hello, World"
-        circleView.addSubview(label)
-        
-        self.view.addSubview(circleView)
-        
-        let squareView = UIView()
-        squareView.frame = CGRect(x: 220, y: 100, width: 100, height: 100)
-        squareView.backgroundColor = .blue
-        
-        let gradientLayer = CAGradientLayer()
-        gradientLayer.frame = squareView.bounds
-        gradientLayer.colors = [UIColor.red.cgColor, UIColor.orange.cgColor]
-        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
-        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
-        squareView.layer.insertSublayer(gradientLayer, at: 0)
-        
-        view.addSubview(squareView)
     }
 
 
