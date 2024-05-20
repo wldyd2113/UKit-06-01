86417c95b968c419c1cb48e6ccbd83d2a6632e8c
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-20 15:51:43 +0900

    PaddingView 추가

--- Text/Text/ViewController.swift
+++ Text/Text/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let paddingView = UIView()
+        paddingView.translatesAutoresizingMaskIntoConstraints = false
+        paddingView.backgroundColor = .blue
+        
         let label = UILabel()
         label.text = "Stylized Text"
         label.font = UIFont.boldSystemFont(ofSize: 17)
@@ -22,13 +26,19 @@ class ViewController: UIViewController {
         label.layer.cornerRadius = 10
         label.clipsToBounds = true
         
-        view.addSubview(label)
+        paddingView.addSubview(label)
+        view.addSubview(paddingView)
 
         NSLayoutConstraint.activate([
-            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            label.topAnchor.constraint(equalTo: view.topAnchor, constant: 150),
-            label.widthAnchor.constraint(equalTo: view.widthAnchor, constant: 200),
-            label.heightAnchor.constraint(equalTo: view.heightAnchor, constant: 40)
+            label.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
+            label.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20),
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            paddingView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            paddingView.topAnchor.constraint(equalTo: view.topAnchor, constant: 150),
+            paddingView.widthAnchor.constraint(equalToConstant: 200),
+            paddingView.heightAnchor.constraint(equalToConstant: 100)
         ])
     }
 
