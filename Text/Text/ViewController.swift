6a5648520eab53cf467dab3df2083ced5074ceea
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-20 16:31:31 +0900

    Clear

--- Text/Text/ViewController.swift
+++ Text/Text/ViewController.swift
@@ -12,48 +12,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let webView = WKWebView()
-        
-        webView.translatesAutoresizingMaskIntoConstraints = false
-        let htmlString = """
-        <html>
-        <head>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <style>
-                body {
-                    font-family: -apple-system, Helvetica, Arial, sans-serif;
-                    font-size: 2rem;
-                    display: flex;
-                    align-items: center;
-                    justify-content: center;
-                }
-                .italic {
-                    font-style: italic;
-                }
-                .blue {
-                    color: blue;
-                }
-                .underline {
-                    text-decoration: underline;
-                }
-                
-            </style>
-        </head>
-        <body>
-            <span class="italic underline">Hello,&nbsp;</span><span class="blue underline">World</span>
-        </body>
-        </html>
-        """
-        webView.loadHTMLString(htmlString, baseURL: nil)
-        
-        view.addSubview(webView)
-        
-        NSLayoutConstraint.activate([
-            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            webView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
-        ])
         
     }
 
