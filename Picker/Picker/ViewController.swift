61894a476097de27b2cbd1f13c2e177d03d3ae3f
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-21 13:37:42 +0900

    Clear

--- Picker/Picker/ViewController.swift
+++ Picker/Picker/ViewController.swift
@@ -7,59 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
-
-    private lazy var pickerVew: UIPickerView = {
-        let pickerView = UIPickerView()
-        pickerView.delegate = self
-        pickerView.dataSource = self
-        pickerView.translatesAutoresizingMaskIntoConstraints = false
-        return pickerView
-    }()
-    let options = ["Option 1", "Option 2", "Option 3", "Option 4", "Option 5"]
-    let options2 = ["옵션 1", "옵션 2", "옵션 3", "옵션 4", "옵션 5"]
-
+class ViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.addSubview(pickerVew)
-        
-        NSLayoutConstraint.activate([
-            pickerVew.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            pickerVew.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        ])
-
     }
-    func numberOfComponents(in pickerView: UIPickerView) -> Int { //피커 수
-        2
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int { //내용물 개수
-        switch component {
-        case 0:
-            return options.count
-        case 1:
-            return options2.count
-        default:
-            return 0
-        }
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        switch component {
-        case 0:
-            return options[row]
-        case 1:
-            return options2[row]
-        default:
-            return ""
-        }
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        print("selected componet: \(component), row: \(row)")
-    }
-
 
 }
 
