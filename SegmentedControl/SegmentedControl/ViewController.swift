48a623498a5450de4b6108d5be01ec7676ae2769
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-21 09:42:47 +0900

    UIButton with configuration

--- SegmentedControl/SegmentedControl/ViewController.swift
+++ SegmentedControl/SegmentedControl/ViewController.swift
@@ -14,14 +14,23 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         let button = UIButton(type: .custom)
-        button.setTitleColor(.white, for: .normal)
-        button.backgroundColor = .systemBlue
-        button.layer.cornerRadius = 10
-        button.layer.borderWidth = 2
-        button.layer.borderColor = UIColor.white.cgColor
-        //button.configuration = UIButton.Configuration.
-        button.contentEdgeInsets = UIEdgeInsets(top: 10, left: 20, bottom: 10, right: 20)
-        button.setTitle("Click me", for: .normal)
+//        button.setTitleColor(.white, for: .normal)
+//        button.backgroundColor = .systemBlue
+//        button.layer.cornerRadius = 10
+//        button.layer.borderWidth = 2
+//        button.layer.borderColor = UIColor.white.cgColor
+        var config = UIButton.Configuration.filled()
+        config.cornerStyle = .dynamic
+        
+        config.title = "Click me"
+        config.baseBackgroundColor = .systemBlue
+        config.baseForegroundColor = .white
+        config.cornerStyle = .capsule
+        
+        config.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20)
+        button.configuration = config
+        
+//        button.setTitle("Click me", for: .normal)
         button.addAction(UIAction { [weak self] _ in
             self?.count += 1
             print("Button was tapped! \(self?.count ?? 0)")
