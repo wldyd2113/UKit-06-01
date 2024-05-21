ffbceaf3d2d7fe9afe30e49cdb123eb87332b28a
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-21 15:19:05 +0900

    Link Button

--- Link/Link/ViewController.swift
+++ Link/Link/ViewController.swift
@@ -11,7 +11,27 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let linkButton = UIButton(type: .system)
+        var conf = UIButton.Configuration.filled()
+        conf.title = "Apple"
+        linkButton.configuration = conf
+        linkButton.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+        
+        linkButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        linkButton.addAction(UIAction { _ in
+            if let url = URL(string: "http://www.apple.com") {
+                UIApplication.shared.open(url)
+            }
+        }, for: .touchUpInside)
+        
+        view.addSubview(linkButton)
+        
+        NSLayoutConstraint.activate([
+            linkButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            linkButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
 
 
