3494c5dff31738c20e54f4a66998cf57fa54c3c6
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-21 11:23:47 +0900

    뷰 컴포넌트 설정 with 스토리보드

--- TextInputApp/TextInputApp/ViewController.swift
+++ TextInputApp/TextInputApp/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var secureTextField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func didTapSubmitButton(_ sender: Any) {
+    }
+    
 }
 
