f1d81f19d865a46fd7fc870a4e2fcb3ba5aece21
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-21 16:45:23 +0900

    UIAlertController 추가

--- Link/Link/ViewController.swift
+++ Link/Link/ViewController.swift
@@ -13,6 +13,40 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let menuButton = UIButton(type: .system)
+        var conf = UIButton.Configuration.filled()
+        conf.title = "Options"
+        menuButton.configuration = conf
+        menuButton.addAction(UIAction { [weak self] _ in
+            self?.showMenu()
+        }, for: .touchUpInside)
+        menuButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(menuButton)
+          NSLayoutConstraint.activate([
+              menuButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+              menuButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+          ])
+    }
+    
+    func showMenu() {
+        let alert = UIAlertController(title: "Options", message: nil, preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "Open", style: .default) { _ in
+            self.showMessage("Open chosen")
+        })
+        alert.addAction(UIAlertAction(title: "Open", style: .default) { _ in
+            self.showMessage("Open chosen")
+        })
+        alert.addAction(UIAlertAction(title: "Open", style: .default) { _ in
+            self.showMessage("Open chosen")
+        })
+        present(alert, animated: true)
+    }
+    
+    func showMessage(_ message: String) {
+        let alert = UIAlertController(title: message, message: nil, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default))
+        present(alert, animated: true)
     }
 
 }
