5d146954cb529caa32715356fb74e5a5f6706768
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-21 14:10:37 +0900

    Clear

--- Picker/Picker/ViewController.swift
+++ Picker/Picker/ViewController.swift
@@ -8,35 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let datePicker = {
-        let picker = UIDatePicker()
-        picker.translatesAutoresizingMaskIntoConstraints = false
-        return picker
-    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let today = Date()
-        var dateComponents = DateComponents()
-        dateComponents.year = -1
-        let oneYearAgo = Calendar.current.date(byAdding: dateComponents, to: today)
-        dateComponents.year = 1
-        let oneYearFromNow = Calendar.current.date(byAdding: dateComponents, to: today)
-        
-        datePicker.minimumDate = oneYearAgo
-        datePicker.maximumDate = oneYearFromNow
-        
-        datePicker.addAction(UIAction {[weak self] _ in
-            print("sender: \(self?.datePicker.date.formatted() ?? "N/A)")")
-        }, for: .valueChanged)
-        
-        view.addSubview(datePicker)
-        
-        NSLayoutConstraint.activate([
-            datePicker.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            datePicker.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        ])
     }
 
 }
