39a329bdb5e1396f05a278be7cae9e9bc8efcbad
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-21 17:23:36 +0900

    서브 메뉴와 팝오버

--- Link/Link/ViewController.swift
+++ Link/Link/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
         conf.title = "Options"
         menuButton.configuration = conf
         menuButton.addAction(UIAction { [weak self] _ in
-            self?.showMenu()
+            self?.showMenu(sourceView: menuButton)
         }, for: .touchUpInside)
         menuButton.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(menuButton)
@@ -29,18 +29,50 @@ class ViewController: UIViewController {
           ])
     }
     
-    func showMenu() {
+    func showMenu(sourceView: UIView) {
         let alert = UIAlertController(title: "Options", message: nil, preferredStyle: .actionSheet)
         alert.addAction(UIAlertAction(title: "Open", style: .default) { _ in
             self.showMessage("Open chosen")
         })
-        alert.addAction(UIAlertAction(title: "Open", style: .default) { _ in
-            self.showMessage("Open chosen")
-        })
-        alert.addAction(UIAlertAction(title: "Open", style: .default) { _ in
-            self.showMessage("Open chosen")
-        })
+        alert.addAction(UIAlertAction(title: "Find", style: .default, handler: { _ in
+            self.showMessage("Find chosen")
+        }))
+        alert.addAction(UIAlertAction(title: "Delete", style: .default, handler: { _ in
+            self.showMessage("Delete chosen")
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+        alert.addAction(UIAlertAction(title: "Canx4cel", style: .default, handler: { [weak self] _ in
+            self?.showSubmenu(sourceView: sourceView)
+        }))
+        
+        // 아이패드에서 팝오버 형태로 서브메뉴 표시
+        if let popoverController = alert.popoverPresentationController {
+            popoverController.sourceView = sourceView
+            popoverController.sourceRect = sourceView.bounds
+            popoverController.permittedArrowDirections = [.down]
+        }
+        
         present(alert, animated: true)
+    }
+    
+    func showSubmenu(sourceView: UIView) {
+        let submenuAlert = UIAlertController(title: "Submenu", message: nil, preferredStyle: .actionSheet)
+        submenuAlert.addAction(UIAlertAction(title: "Copy Format", style: .default, handler: { _ in
+            self.showMessage("Copy format chosen")
+        }))
+        submenuAlert.addAction(UIAlertAction(title: "Paste Format", style: .default, handler: { _ in
+            self.showMessage("Paste Format chosen")
+        }))
+        submenuAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        // 아이패드에서 팝오버 형태로 서브메뉴 표시
+        if let popoverController = submenuAlert.popoverPresentationController {
+            popoverController.sourceView = sourceView
+            popoverController.sourceRect = sourceView.bounds
+            popoverController.permittedArrowDirections = [.up]
+        }
+        
+        present(submenuAlert, animated: true, completion: nil)
     }
     
     func showMessage(_ message: String) {
