f55832b7cdff5e564a61a14e4c8f272d5b2d8ee5
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-22 13:28:31 +0900

    DatePicker 만들어보기

--- CustomDatePicker/CustomDatePicker/ViewController.swift
+++ CustomDatePicker/CustomDatePicker/ViewController.swift
@@ -7,11 +7,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    
+    private lazy var datePicker: UIPickerView = {
+        let picker = UIPickerView()
+        picker.delegate = self
+        picker.dataSource = self
+        return picker
+    }()
+    
+    let years = [Int](2000...2050)
+    let months = [Int](1...12)
+    let days = [Int](1...31)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        datePicker.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(datePicker)
+        
+        NSLayoutConstraint.activate([
+            datePicker.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            datePicker.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        3
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        switch component {
+        case 0:
+            return years.count
+        case 1:
+            return months.count
+        case 2:
+            return days.count
+        default:
+            return 0
+        }
+    }
+    
+    // MARK: - UIPickerViewDelegate
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        switch component {
+        case 0:
+            return String(describing:years[row])
+        case 1:
+            return String(describing:months[row])
+        case 2:
+            return String(describing:days[row])
+        default:
+            return nil
+        }
     }
 
 
