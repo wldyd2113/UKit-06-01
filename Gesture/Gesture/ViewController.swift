fe8deda2d11d6db1bb40d581280da2a2eafea8a2
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-22 10:36:05 +0900

    Rotation

--- Gesture/Gesture/ViewController.swift
+++ Gesture/Gesture/ViewController.swift
@@ -12,17 +12,20 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         let imageView = UIImageView(image: UIImage(systemName: "star.fill"))
-        imageView.frame = CGRect(x: 120, y: 300, width: 200, height: 200)
+        imageView.frame = CGRect(x: view.bounds.midX-100, y: view.bounds.midY-100, width: 200, height: 200)
         imageView.isUserInteractionEnabled = true
         
         view.addSubview(imageView)
         
-        let gesture = UIPinchGestureRecognizer(target: self, action: #selector(handleGesture))
+        let gesture = UIRotationGestureRecognizer(target: self, action: #selector(handleGesture))
         imageView.addGestureRecognizer(gesture)
     }
 
-    @objc func handleGesture(_ sender: UIPinchGestureRecognizer) {
-
+    @objc func handleGesture(_ sender: UIRotationGestureRecognizer) {
+        if let view = sender.view {
+            view.transform = view.transform.rotated(by: sender.rotation)
+            sender.rotation = 0
+        }
     }
 }
 
