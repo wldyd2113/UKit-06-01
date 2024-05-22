1315abf68935ac6f62912dab6c61dbd7f65fa1ff
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-22 14:27:50 +0900

    단순한 목록 with TableView

--- List/List/ViewController.swift
+++ List/List/ViewController.swift
@@ -7,11 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    let items = ["고양이", "강아지", "새", "파충류", "물고기"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let tableView = UITableView(frame: view.bounds)
+        tableView.dataSource = self
+        view.addSubview(tableView)
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") ?? UITableViewCell(style: .default, reuseIdentifier: "cell")
+        var config = cell.defaultContentConfiguration()
+        config.text = items[indexPath.row]
+        cell.contentConfiguration = config
+        return cell
     }
 
 
