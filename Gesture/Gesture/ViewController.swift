bb725100fa37fa1f07c2f4183c711abbd22f62e4
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-22 09:27:04 +0900

    TapGesture

--- Gesture/Gesture/ViewController.swift
+++ Gesture/Gesture/ViewController.swift
@@ -11,7 +11,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let rectangle = UIView()
+        rectangle.backgroundColor = .yellow
+        rectangle.frame = CGRect(x: 100, y: 100, width: 175, height: 125)
+        rectangle.isUserInteractionEnabled = true
+        
+        view.addSubview(rectangle)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        tapGesture.numberOfTapsRequired = 2 //tap을 하는 최소한의 횟수
+        rectangle.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func handleTap(_ sender: UITapGestureRecognizer) {
+        if let view = sender.view {
+            view.backgroundColor = (view.backgroundColor == .yellow) ? .red : .yellow
+        }
     }
 
 
