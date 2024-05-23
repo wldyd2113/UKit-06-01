a7b0566bc38143b5b3aa3f361caa69b5b3d87baf
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-23 16:49:13 +0900

    페이지 전환

--- NavigationController/NavigationController/ViewController.swift
+++ NavigationController/NavigationController/ViewController.swift
@@ -27,6 +27,16 @@ class ViewController: UIViewController {
         let extraButton = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(extraButtonTapped))
         
         self.navigationItem.rightBarButtonItems = [rightButton, extraButton]
+        
+        let button = UIButton(type: .system)
+        button.setTitle("다음 화면으로", for: .normal)
+        button.addAction(UIAction{ [weak self] _ in
+            let nextViewController = UIViewController()
+            nextViewController.view.backgroundColor = .white
+            self?.show(nextViewController, sender: nil)
+        }, for: .touchUpInside)
+        button.frame = CGRect(x: 100, y: 100, width: 200, height: 50)
+        view.addSubview(button)
     }
     
     @objc func leftButtonTapped() {
