0d897dba81ef660e2bbcab8ccb429a504db01112
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-23 11:28:16 +0900

    오류 수정

--- Form/Form/ViewController.swift
+++ Form/Form/ViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
     let formOneLabel = UILabel()
     let formOneTextField = UITextField()
     let formTwoLabel = UILabel()
@@ -16,53 +15,50 @@ class ViewController: UIViewController {
     let resultLabelOne = UILabel()
     let resultLabelTwo = UILabel()
     lazy var textFieldAction = UIAction(handler: textFieldDidChange)
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = .white
-
+        
         setupFormOne()
         setupFormTwo()
         setupResults()
         
-//        formOneTextField.addAction(UIAction {[weak self] _ in
+//        formOneTextField.addAction(UIAction { [weak self] _ in
 //            self?.resultLabelOne.text = "폼 #1 = \(self?.formOneTextField.text ?? "")"
 //        }, for: .editingChanged)
-//        
-//        formTwoTextField.addAction(UIAction {[weak self] _ in
+//
+//        formTwoTextField.addAction(UIAction { [weak self] _ in
 //            self?.resultLabelTwo.text = "폼 #2 = \(self?.formTwoTextField.text ?? "")"
 //        }, for: .editingChanged)
+        
 //        formOneTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
 //        formTwoTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
-//        formOneTextField.addAction(UIAction(handler:textFieldDidChange), for: .editingChanged)
-//        formTwoTextField.addAction(UIAction(handler:textFieldDidChange), for: .editingChanged)
-        
-        // 액션 개체 생성 / 삭제 코드 추가
-        override func viewIsAppearing(_ animated: Bool) {
-            super.viewIsAppearing(animated)
-            formOneTextField.addAction(textFieldAction, for: .editingChanged)
-            formTwoTextField.addAction(textFieldAction, for: .editingChanged)
-        }
-
-        override func viewWillDisappear(_ animated: Bool) {
-            super.viewWillDisappear(animated)
-            formOneTextField.removeAction(textFieldAction, for: .editingChanged)
-            formTwoTextField.removeAction(textFieldAction, for: .editingChanged)
-        }
-
     }
-
+    
+    // 액션 개체 생성 / 삭제 코드 추가
+    override func viewIsAppearing(_ animated: Bool) {
+        super.viewIsAppearing(animated)
+        formOneTextField.addAction(textFieldAction, for: .editingChanged)
+        formTwoTextField.addAction(textFieldAction, for: .editingChanged)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        formOneTextField.removeAction(textFieldAction, for: .editingChanged)
+        formTwoTextField.removeAction(textFieldAction, for: .editingChanged)
+    }
+    
     func setupFormOne() {
         formOneLabel.text = "이것은 첫 번째 폼입니다"
         formOneLabel.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(formOneLabel)
-
+        
         formOneTextField.borderStyle = .roundedRect
         formOneTextField.placeholder = "여기에 입력하세요"
         formOneTextField.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(formOneTextField)
-
+        
         NSLayoutConstraint.activate([
             formOneLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
             formOneLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
@@ -70,19 +66,18 @@ class ViewController: UIViewController {
             formOneTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             formOneTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
         ])
-
     }
-
+    
     func setupFormTwo() {
         formTwoLabel.text = "이것은 두 번째 폼입니다"
         formTwoLabel.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(formTwoLabel)
-
+        
         formTwoTextField.borderStyle = .roundedRect
         formTwoTextField.placeholder = "여기에 입력하세요"
         formTwoTextField.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(formTwoTextField)
-
+        
         NSLayoutConstraint.activate([
             formTwoLabel.topAnchor.constraint(equalTo: formOneTextField.bottomAnchor, constant: 20),
             formTwoLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
@@ -91,14 +86,14 @@ class ViewController: UIViewController {
             formTwoTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
         ])
     }
-
+    
     func setupResults() {
         resultLabelOne.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(resultLabelOne)
-
+        
         resultLabelTwo.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(resultLabelTwo)
-
+        
         NSLayoutConstraint.activate([
             resultLabelOne.topAnchor.constraint(equalTo: formTwoTextField.bottomAnchor, constant: 20),
             resultLabelOne.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
@@ -116,6 +111,7 @@ class ViewController: UIViewController {
 //    }
     func textFieldDidChange(_ action: UIAction) {
         guard let textField = action.sender as? UITextField else { return }
+        
         if textField == formOneTextField {
             resultLabelOne.text = "폼 #1 = \(textField.text ?? "")"
         } else {
