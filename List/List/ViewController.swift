4f0c63065b194ff82e59146cfb43e0487f3760d6
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-23 09:39:41 +0900

    오류수정

--- List/List/ViewController.swift
+++ List/List/ViewController.swift
@@ -16,32 +16,32 @@ class ViewController: UIViewController, UITableViewDataSource {
     let animals = [
         Animal(name: "고양이", image: UIImage(systemName: "cat")!),
         Animal(name: "강아지", image: UIImage(systemName: "dog")!),
-        Animal(name: "토끼", image: UIImage(systemName: "hare")!),
-        
+        Animal(name: "토끼", image: UIImage(systemName: "hare")!)
     ]
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         let tableView = UITableView(frame: view.bounds)
         tableView.dataSource = self
-        tableView.register(UITableView.self, forCellReuseIdentifier: "cell")
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         view.addSubview(tableView)
-        
     }
-    
+
+    // MARK: - UITableViewDataSource
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         animals.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        var config  = cell.defaultContentConfiguration()
+        var config = cell.defaultContentConfiguration()
         config.text = animals[indexPath.row].name
         config.image = animals[indexPath.row].image
         cell.contentConfiguration = config
         return cell
     }
+
 
 }
 
