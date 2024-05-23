3e067f924f1aa8e5c81df431c5be83c0bdf73590
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-23 09:20:16 +0900

    Clear

--- List/List/ViewController.swift
+++ List/List/ViewController.swift
@@ -7,54 +7,12 @@
 
 import UIKit
 
-struct Animal {
-    let name: String
-}
-
-struct AnimalCategory {
-    let category: String
-    let animals: [Animal]
-}
-
-class ViewController: UIViewController, UITableViewDataSource {
-
-    let categories = [
-        AnimalCategory(category: "포유류", animals: [Animal(name: "고양이"), Animal(name: "강아지")]),
-        AnimalCategory(category: "파충류", animals: [Animal(name: "거북이"), Animal(name: "도마뱀")])
-
-    ]
+class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // 테이블 스타일 변경 가능 (생성자 함수에서)
-        let tableView = UITableView(frame: view.bounds, style: .insetGrouped)
-        tableView.dataSource = self
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        view.addSubview(tableView)
-        
     }
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return categories.count
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return categories[section].animals.count
-    }
-    
-    func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
-        return categories[section].category
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") ?? UITableViewCell(style: .default, reuseIdentifier: "cell")
-        var config = cell.defaultContentConfiguration()
-        config.text = categories[indexPath.section].animals[indexPath.row].name
-        cell.contentConfiguration = config
-        return cell
-    }
-
 
 }
 
